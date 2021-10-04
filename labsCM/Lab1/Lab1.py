93d625aec89cbbec1ec0678e68ac04c70812c2ce
= <=>
2021-10-04 20:36:01 +0300

    chorde_method add

--- labsCM/Lab1/Lab1.py
+++ labsCM/Lab1/Lab1.py
@@ -1,4 +1,5 @@
 import os
+#test 4,-3,1,2,-4,6  [-2,0]
 def function(сoefecients, x):
     return coeficients[0]+coeficients[1]*x+coeficients[2]*(x**2)+coeficients[3]*(x**3)+coeficients[4]*(x**4)+coeficients[5]*(x**5)
 
@@ -16,6 +17,20 @@ def bisection_method(range_start, range_end, coeficients):
     else:
         return range_middle
 
+def chorde_method(range_start, range_end, coeficients):
+    global chorde_method_iterations
+    secant_x = (range_start*function(coeficients, range_end)-range_end*function(coeficients, range_start))/(function(coeficients, range_end)-function(coeficients, range_start))
+    if(abs(secant_x - range_start) > POHIBKA):
+        chorde_method_iterations += 1
+        print("Метод хорд " + str(chorde_method_iterations) + "-ая итерация c = " + str(secant_x))
+        file.write("Метод хорд " + str(chorde_method_iterations) + "-ая итерация c = " + str(secant_x) + '\n')
+        if (function(coeficients, secant_x) >= POHIBKA):
+            return bisection_method(range_start,secant_x,coeficients)
+        elif (function(coeficients, secant_x) <= -POHIBKA):
+            return bisection_method(secant_x,range_end,coeficients)
+    else:
+        return secant__x
+    
 
 with open('labsCM\console.txt', 'w') as file:
     print("Рішення рівняння виду a5*х^5+a4*x^4+a3*x^3+a2*x^2+a1*x+a0")
@@ -28,5 +43,8 @@ with open('labsCM\console.txt', 'w') as file:
     POHIBKA = 1E-5
     bisection_method_iterations = 0
     bisection_method_result = bisection_method(range_start, range_end, coeficients)
+    chorde_method_iterations = 0
+    chorde_method_result = chorde_method(range_start, range_end, coeficients)
+
 os.rename('labsCM\console.txt','labsCM\Result.txt')
 
