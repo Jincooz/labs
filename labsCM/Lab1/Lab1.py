a5d19de5d38956ab0c1be20300fc7c01242d847d
= <=>
2021-10-04 20:14:27 +0300

    Lab1 folder creation

--- labsCM/Lab1/Lab1.py
+++ labsCM/Lab1/Lab1.py
@@ -1,3 +1,4 @@
+import os
 def function(сoefecients, x):
     return coeficients[0]+coeficients[1]*x+coeficients[2]*(x**2)+coeficients[3]*(x**3)+coeficients[4]*(x**4)+coeficients[5]*(x**5)
 
@@ -6,8 +7,9 @@ def bisection_method(range_start, range_end, coeficients):
     range_middle = (range_start + range_end) / 2
     if(abs(range_middle - range_start) > POHIBKA):
         bisection_method_iterations += 1
+        print("Метод бисекций " + str(bisection_method_iterations) + "-ая итерация c = " + str(range_middle))
+        file.write("Метод бисекций " + str(bisection_method_iterations) + "-ая итерация c = " + str(range_middle) + '\n')
         if (function(coeficients, range_middle) >= POHIBKA):
-            print("Метод бисекций " + bisection_method_iterations + "-ая итерация c = " +)
             return bisection_method(range_start,range_middle,coeficients)
         elif (function(coeficients, range_middle) <= -POHIBKA):
             return bisection_method(range_middle,range_end,coeficients)
@@ -15,8 +17,7 @@ def bisection_method(range_start, range_end, coeficients):
         return range_middle
 
 
-
-with open('con.txt', 'w') as file:
+with open('labsCM\console.txt', 'w') as file:
     print("Рішення рівняння виду a5*х^5+a4*x^4+a3*x^3+a2*x^2+a1*x+a0")
     coeficients = input("Введіть коефіцієнти у форматі a5,a4,a3,a2,a1,a0 : ").split(',')
     for i in range(0,6):
@@ -27,4 +28,5 @@ with open('con.txt', 'w') as file:
     POHIBKA = 1E-5
     bisection_method_iterations = 0
     bisection_method_result = bisection_method(range_start, range_end, coeficients)
+os.rename('labsCM\console.txt','labsCM\Result.txt')
 
