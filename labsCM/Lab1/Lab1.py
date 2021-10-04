dc115cf1c05312b243fc951d4717173626b472ac
= <=>
2021-10-04 23:02:48 +0300

    end

--- labsCM/Lab1/Lab1.py
+++ labsCM/Lab1/Lab1.py
@@ -1,50 +1,27 @@
+import methods as mth
 import os
 #test 4,-3,1,2,-4,6  [-2,0]
-def function(сoefecients, x):
-    return coeficients[0]+coeficients[1]*x+coeficients[2]*(x**2)+coeficients[3]*(x**3)+coeficients[4]*(x**4)+coeficients[5]*(x**5)
 
-def bisection_method(range_start, range_end, coeficients):
-    global bisection_method_iterations
-    range_middle = (range_start + range_end) / 2
-    if(abs(range_middle - range_start) > POHIBKA):
-        bisection_method_iterations += 1
-        print("Метод бисекций " + str(bisection_method_iterations) + "-ая итерация c = " + str(range_middle))
-        file.write("Метод бисекций " + str(bisection_method_iterations) + "-ая итерация c = " + str(range_middle) + '\n')
-        if (function(coeficients, range_middle) >= POHIBKA):
-            return bisection_method(range_start,range_middle,coeficients)
-        elif (function(coeficients, range_middle) <= -POHIBKA):
-            return bisection_method(range_middle,range_end,coeficients)
-    else:
-        return range_middle
-
-def chorde_method(range_start, range_end, coeficients):
-    global chorde_method_iterations
-    secant_x = (range_start*function(coeficients, range_end)-range_end*function(coeficients, range_start))/(function(coeficients, range_end)-function(coeficients, range_start))
-    if(abs(secant_x - range_start) > POHIBKA):
-        chorde_method_iterations += 1
-        print("Метод хорд " + str(chorde_method_iterations) + "-ая итерация c = " + str(secant_x))
-        file.write("Метод хорд " + str(chorde_method_iterations) + "-ая итерация c = " + str(secant_x) + '\n')
-        if (function(coeficients, secant_x) >= POHIBKA):
-            return bisection_method(range_start,secant_x,coeficients)
-        elif (function(coeficients, secant_x) <= -POHIBKA):
-            return bisection_method(secant_x,range_end,coeficients)
-    else:
-        return secant__x
-    
-
+os.remove('labsCM\Lab1\Result.txt')
 with open('labsCM\Lab1\console.txt', 'w') as file:
     print("Рішення рівняння виду a5*х^5+a4*x^4+a3*x^3+a2*x^2+a1*x+a0")
-    coeficients = input("Введіть коефіцієнти у форматі a5,a4,a3,a2,a1,a0 : ").split(',')
+    coefficients = input("Введіть коефіцієнти у форматі a5,a4,a3,a2,a1,a0 : ").split(',')
     for i in range(0,6):
-        coeficients[i]=int(coeficients[i])
-    coeficients = coeficients[::-1]
-    range = input("Vvedite promizok in format [a,b]: ")[1:-1].split(',')
+        coefficients[i]=int(coefficients[i])
+    coefficients = coefficients[::-1]
+    range = input("Введіть проміжок в форматі [a,b]: ")[1:-1].split(',')
     range_start, range_end = float(range[0]), float(range[1])
-    POHIBKA = 1E-5
-    bisection_method_iterations = 0
-    bisection_method_result = bisection_method(range_start, range_end, coeficients)
-    chorde_method_iterations = 0
-    chorde_method_result = chorde_method(range_start, range_end, coeficients)
+    precision = 1E-5
+    mth.bisection_method_iterations = 0
+    mth.bisection_method_result = mth.bisection_method(range_start, range_end, coefficients, precision, file)
+    print('------------------------')
+    file.write('------------------------')
+    mth.chorde_method_iterations = 0
+    mth.chorde_method_result = mth.chorde_method(range_start, range_end, coefficients, precision, file)
+    print('------------------------')
+    file.write('------------------------')
+    mth.newton_method_iterations = 0
+    mth.newton_method_result = mth.newton_method(range_start, range_end, coefficients, precision, file)
 
 os.rename('labsCM\Lab1\console.txt','labsCM\Lab1\Result.txt')
 
---
+++ labsCM/Lab1/methods.py
@@ -0,0 +1,57 @@
+def function(coefficients, x):
+    return coefficients[0] + coefficients[1] * x + coefficients[2] * (x**2) + coefficients[3] * (x**3) \
+        + coefficients[4] * (x**4) + coefficients[5] * (x**5)
+
+def function_derivative(coefficients, x):
+    return coefficients[1] + 2 * coefficients[2] * x + 3 * coefficients[3] * (x**2) \
+        + 4 * coefficients[4] * (x**3) + 5 * coefficients[5] * (x**4)
+
+def function_derivative2(coefficients, x):
+    return 2 * coefficients[2] + 2 * 3 * coefficients[3] * x + 3 * 4 * coefficients[4] * (x**2) + 4 * 5 * coefficients[5] * (x**3)
+
+def bisection_method(range_start, range_end, coefficients, precision, file):
+    global bisection_method_iterations
+    range_middle = (range_start + range_end) / 2
+    if(abs(range_middle - range_start) > precision):
+        bisection_method_iterations += 1
+        print("Метод бисекций " + str(bisection_method_iterations) + "-а ітерація c = " + str(range_middle))
+        file.write("Метод бисекций " + str(bisection_method_iterations) + "-а ітерація c = " + str(range_middle) + '\n')
+        if (function(coefficients, range_middle) >= precision):
+            return bisection_method(range_start,range_middle,coefficients, precision, file)
+        elif (function(coefficients, range_middle) <= -precision):
+            return bisection_method(range_middle,range_end,coefficients, precision, file)
+    else:
+        return range_middle
+
+def chorde_method(range_start, range_end, coefficients, precision, file):
+    global chorde_method_iterations
+    secant_x = (range_start*function(coefficients, range_end)-range_end*function(coefficients, range_start))/(function(coefficients, range_end)-function(coefficients, range_start))
+    if(abs(secant_x - range_start) > precision):
+        chorde_method_iterations += 1
+        print("Метод хорд " + str(chorde_method_iterations) + "-а ітерація c = " + str(secant_x))
+        file.write("Метод хорд " + str(chorde_method_iterations) + "-а ітерація c = " + str(secant_x) + '\n')
+        if (function(coefficients, secant_x) >= precision):
+            return chorde_method(range_start,secant_x,coefficients, precision, file)
+        elif (function(coefficients, secant_x) <= -precision):
+            return chorde_method(secant_x,range_end,coefficients, precision, file)
+    else:
+        return secant_x
+
+def newton_method(range_start, range_end, coefficients, precision, file):
+    global newton_method_iterations
+    if ((function_derivative(coefficients,range_start) > 0 and function_derivative2(coefficients, range_start) > 0) or\
+        (function_derivative(coefficients,range_start) < 0 and function_derivative2(coefficients, range_start) < 0)):
+        x_prev = range_end
+    else:
+        x_prev = range_start
+    x = x_prev - (function(coefficients,x_prev)/function_derivative(coefficients,x_prev))
+    newton_method_iterations += 1
+    print("Метод ньютона " + str(newton_method_iterations) + "-а ітерація x = " + str(x))
+    file.write("Метод ньютона " + str(newton_method_iterations) + "-а ітерація x = " + str(x) + '\n')
+    while(abs(x-x_prev) > precision and abs(function(coefficients,x)) > precision):
+        newton_method_iterations += 1
+        x_prev = x
+        x = x_prev - (function(coefficients,x_prev)/function_derivative(coefficients,x_prev))
+        print("Метод ньютона " + str(newton_method_iterations) + "-а ітерація x = " + str(x))
+        file.write("Метод ньютона " + str(newton_method_iterations) + "-а ітерація x = " + str(x) + '\n')
+    return x
