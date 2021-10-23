80a0d9691256336c0a5759929622a8d54d1173b6
Jincooz <shaddyreaper@gmail.com>
2021-10-23 18:49:25 +0300

    add U,transpoe,determinant,multiply

--- labsCM/Lab2/Lab2.py
+++ labsCM/Lab2/Lab2.py
@@ -1,10 +1,49 @@
 import os
+import math
 
 def dprint(string):
     global outfile
     print(string)
     outfile.write(string)
     pass
+
+def U(matrix):
+    U = [[complex(0,0) for element in row] for row in matrix]
+    for i in range(matrix.__len__()):
+        sum = 0
+        for k in range(0,matrix.__len__()):
+            sum += U[k][i] * U[k][i]
+        if(matrix[i][i] >= sum.real):
+            U[i][i] = complex(math.sqrt(matrix[i][i] - sum.real),0)
+        else:
+            U[i][i] = complex(0,math.sqrt( - matrix[i][i] + sum.real))
+        for j in range(matrix.__len__()):
+            sum = 0
+            for k in range(0,i):
+                sum += U[k][i] * U[k][j]
+            U[i][j] = (matrix[i][j] - sum) / U[i][i]
+    return U
+
+def transpose(matrix):
+    return [[matrix[j][i] for j in range(matrix.__len__())] for i in range(matrix.__len__())]
+
+def determinant(matrix):
+    result = 1
+    for i in range(matrix.__len__()):
+        result *= matrix[i][i]
+    return result
+
+def multiply(matrix1, matrix2):
+    if(matrix1[0].__len__()!=matrix2.__len__()):
+        return -1
+    n =  matrix2.__len__()
+    ResultMatrix = [[complex(0,0) for element in matrix2[0]] for row in matrix1]
+    for i in range(ResultMatrix.__len__()):
+        for j in range(ResultMatrix.__len__()):
+            for l in range(n):
+                ResultMatrix[i][j] += matrix1[i][l]*matrix2[l][j] 
+    return ResultMatrix
+
 
 def main():
     with open('labsCM\Lab2\Input.txt', 'r') as infile:
@@ -17,8 +56,12 @@ def main():
         vector.append(row.pop(-1))        
     print(vector)
     print(matrix)
-    
-
+    Umatrix = U(matrix)
+    print(Umatrix)
+    TUmatrix = transpose(Umatrix)
+    print(TUmatrix)
+    multipy_result = multiply(TUmatrix,Umatrix)
+    pass
 
 os.remove('labsCM\Lab2\Console.txt')
 with open('labsCM\Lab2\Console.txt', 'w') as outfile:
