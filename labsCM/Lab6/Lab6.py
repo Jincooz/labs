00765f4aa3befe99e278dfa70ce889ef203b530e
Jincooz <shaddyreaper@gmail.com>
2022-04-22 19:08:00 +0300

    end of lab 6

--- labsCM/Lab6/Lab6.py
+++ labsCM/Lab6/Lab6.py
@@ -1,9 +1,12 @@
 import math as mt
 import random as rng
+from tkinter import E
+import numpy as np
+from matplotlib import pyplot as plt
 
 #TODO changee function to pow(7,5*x)
 def function(x):
-    return x
+    return pow(7,x)
 
 def create_basic_polynomial(x_list, i):
     def basic_polynomial(x):
@@ -55,6 +58,45 @@ def create_newtons_polynomial(x_list, y_list):
         return result
     return newtons_polynomial
 
+def create_spline(x_list, y_list):
+    h = np.zeros(len(x_list) - 1)
+    n = np.zeros(len(y_list) - 1)
+    for i in range(len(x_list) - 1):
+        h[i] = x_list[i+1] - x_list[i]
+        n[i] = y_list[i+1] - y_list[i]
+    matrix = np.zeros((len(x_list)-2,len(x_list)-2))
+    result_vector = np.zeros((len(x_list))-2)
+    matrix[0][0] = 2 * (h[0] + h[1])
+    matrix[0][1] = h[1]
+    result_vector[0] = 6 * (n[1]/h[1] - n[0]/h[0])
+    for i in range(1,len(h)-2):
+        matrix[i][i-1] = h[i]
+        matrix[i][i] = 2 * (h[i] + h[i+1])
+        matrix[i][i+1] = h[i+1]
+        result_vector[i] = 6 * (n[i+1]/h[i+1]-n[i]/h[i])
+    matrix[len(h) - 2][len(h) - 2] = 2 * (h[len(h)-2] + h[len(h)-1])
+    matrix[len(h) - 2][len(h) - 3] = h[len(h)-1]
+    result_vector[len(h) - 2] = 6 * (n[len(h) - 1]/h[len(h) - 1]-n[len(h) - 2]/h[len(h) - 2])
+    c = np.linalg.solve(matrix, result_vector)
+    c = np.insert(c, 0, 0)
+    d= np.zeros(len(h))
+    b = np.zeros(len(h))
+    a = np.zeros(len(h))
+    for i in range(len(x_list) - 2):
+        a[i] = y_list[i]
+        d[i] = ((c[i+1] - c[i])/(3*h[i]))
+        b[i] = ((y_list[i+1]-y_list[i])/h[i] - c[i]*h[i] - d[i]*h[i]*h[i])
+    a[len(h)-1] = y_list[len(h)-1]
+    d[len(h)-1] = (-1*c[len(h)-1]/(3*h[len(h)-1]))
+    b[len(h)-1] = (-2*h[len(h)-1]*c[len(h)-1]/3 + (y_list[len(h)]-y_list[len(h)-1])/h[len(h)-1])
+    def spline(x):
+        i = 0
+        while x_list[i+1]+0.00000001 < x:
+            i += 1
+        dx = x-x_list[i]
+        return a[i] + b[i] * dx + c[i] * dx * dx + d[i] * dx * dx * dx
+    return spline
+
 def console_input_get_x_lists_console():
     amount_of_dots = int(input("Amount of dots : "))
     x_list = []
@@ -91,21 +133,25 @@ def polinomial_list_initialize(x_list, y_list):
     polinomial_list.append(create_lagrange_polynomial(x_list, y_list))
     polinomial_list.append(create_newtons_polynomial(x_list, y_list))
     polinomial_list.append(create_newtons_polynomial(x_list[::-1], y_list[::-1]))
+    polinomial_list.append(create_spline(x_list, y_list))
     return polinomial_list
 
 def test_print(x_list, polinomial_list):
     sumdy = [0 for i in range(len(polinomial_list))]
-    for x in x_list:
-        dy = [abs(polinomial_list[i](x)-function(x)) for i in range(len(polinomial_list))]
-        for i in range(len(polinomial_list)):
-            sumdy[i] += dy[i]
+    e = np.zeros((len(polinomial_list),len(x_list)))
+    for i,x in enumerate(x_list):
+        dy = [interpol_func(x)-function(x) for interpol_func in polinomial_list]
+        for j in range(len(polinomial_list)):
+            sumdy[j] += abs(dy[j])
+            e[j][i] = abs(dy[j])
         print("x = %.5f" % (x), end="")
-        for i in range(len(polinomial_list)):
-            print(" y%i = %s" % (i, ("yes" if  dy[i] < 0.000001 else "no")), end="")
+        for j in range(len(polinomial_list)):
+            print(" y%i = %s" % (j, ("yes" if  abs(dy[j]) < 0.000001 else "no")), end="")
         print("")
     for i in range(len(polinomial_list)):
         print("y%i = %.5f " % (i, sumdy[i]), end="")
     print("")
+    return e
 
 def main():
     print("How to get x list?\n1. Hand input of dots coordinates\n2. Linear automatic\n3. Power automatic\n4. Exit")
@@ -118,10 +164,22 @@ def main():
         return 0
     else:
         print("Invalid input. Try again later." )
-        return        
+        return
     y_list = [function(x) for x in x_list]
     polinomial_list = polinomial_list_initialize(x_list, y_list)
     test_print(x_list, polinomial_list)
-    test_print(x_list2, polinomial_list)
+    e = test_print(x_list2, polinomial_list)
+    plt.rcParams['figure.figsize'] = [20, 5]
+    plt.plot(x_list2, e[0])
+    plt.plot(x_list2, e[1])
+    plt.plot(x_list2, e[2])
+    plt.plot(x_list2, e[3])
+    plt.legend(['lagrange',"newton","reverse newton", "spline"])
+    plt.ylabel("e")
+    plt.xlabel("x")
+    plt.yscale("log")
+    plt.tick_params(axis = 'x', labelrotation = 90, labelsize = 9)
+    plt.show()
+
 
 main()
