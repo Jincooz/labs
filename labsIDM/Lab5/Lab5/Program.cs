9e4edb1e29b2a8c00fdf9870b0f998026210d15a
Jincooz <shaddyreaper@gmail.com>
2022-05-01 20:28:28 +0300

    NeuralNetwork TrainingData class

--- labsIDM/Lab5/Lab5/Program.cs
+++ labsIDM/Lab5/Lab5/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using num = MathNet.Numerics;
 using linalg = MathNet.Numerics.LinearAlgebra;
-Console.WriteLine("Hello, World!");
 int[] amountOfNodes = { 3, 2 };
 Lab5.NeuralNetwork neuralNetwork = new Lab5.NeuralNetwork(amountOfNodes);
 double[] vs = { 1d, 2d, 3d };
@@ -12,6 +11,62 @@ namespace Lab5
 {
     class NeuralNetwork
     {
+        private class TrainingData
+        {
+            private readonly int _inputNodesAmount, _outputNodesAmount;
+            private readonly int _amountOfRecordsInDataset;
+            private readonly List<linalg.Vector<double>[]> _data;
+            private int _amountOfData;
+            private List<List<linalg.Vector<double>[]>> _datasets;
+            private int _amountOFDatasets;
+            public int AmountOfRecordsInDataset { get { return _amountOfRecordsInDataset; } }
+            public List<List<linalg.Vector<double>[]>> DataSets { get { return _datasets; } }
+            public int CountDatasets { get { return _amountOFDatasets; } }
+            public TrainingData(linalg.Matrix<double> trainingData, int groupBy, int inputNodesAmount, int outputNodesAmount)
+            {
+                _amountOfRecordsInDataset = groupBy;
+                _inputNodesAmount = inputNodesAmount;
+                _outputNodesAmount = outputNodesAmount;
+                _amountOfData = 0;
+                _amountOFDatasets = 0;
+                _data = new List<linalg.Vector<double>[]>();
+                AddData(trainingData);
+            }
+            public void AddData(linalg.Matrix<double> newTrainingData)
+            {
+                for (int i = 0; i < newTrainingData.RowCount; i++)
+                {
+                    linalg.Vector<double>[] newRecord = new linalg.Vector<double>[2];
+                    newRecord[0] = linalg.Vector<double>.Build.Dense(_inputNodesAmount, 0);
+                    newRecord[1] = linalg.Vector<double>.Build.Dense(_outputNodesAmount, 0);
+                    for (int j = 0; j < _inputNodesAmount + _outputNodesAmount; j++)
+                    {
+                        if (i < _inputNodesAmount)
+                            newRecord[0][j] = newTrainingData[i, j];
+                        else
+                            newRecord[1][j] = newTrainingData[i, j];
+                    }
+                    _data.Add(newRecord);
+                    _amountOfData++;
+                }
+                DivideDataInDatasets();
+            }
+            private void DivideDataInDatasets()
+            {
+                _datasets = new List<List<linalg.Vector<double>[]>>();
+                for (int i = 0; i < _amountOfData / _amountOfRecordsInDataset; i++)
+                {
+                    List<linalg.Vector<double>[]> newDataset = new List<linalg.Vector<double>[]>();
+                    for (int j = 0; j < _amountOfRecordsInDataset; j++)
+                    {
+                        newDataset.Add(_data[i * _amountOfRecordsInDataset + j]);
+                    }
+                    _datasets.Add(newDataset);
+                    _amountOFDatasets++;
+                }
+            }
+        }
+        private TrainingData _trainingData;
         private readonly List<int>? _amountOfNodes;
         private List<linalg.Matrix<double>> _weightMatrices = new List<linalg.Matrix<double>>();
         private List<linalg.Vector<double>> _biases = new List<linalg.Vector<double>>();
@@ -20,11 +75,20 @@ namespace Lab5
         private linalg.Vector<double>? _outputLayer;
         public linalg.Vector<double>? InputLayer { get { return _inputLayer; } set { _inputLayer = value; } }
         public linalg.Vector<double>? OutputLayer { get { return _outputLayer; } }
-        private linalg.Matrix<double> XavierWeightInitialization(int numberOfNeronsInPreviusLayer, int numberOfNeronsInThisLayer, Random rand)
+        private linalg.Matrix<double> XavierWeightInitialization(int numberOfNeronsInPreviusLayer, int numberOfNeronsInThisLayer)
         {
             //uniform probability distribution in (-1/sqrt(n),1/sqrt(n)), where n - amount of neuron in previuse layer
+            Random rand = new Random(DateTime.Now.Second);
             double lower = -(1.0 / Math.Sqrt(numberOfNeronsInPreviusLayer)), upper = (1.0 / Math.Sqrt(numberOfNeronsInPreviusLayer));
-            return linalg.Matrix<double>.Build.Dense(numberOfNeronsInThisLayer, numberOfNeronsInPreviusLayer, lower + rand.NextDouble() * (upper - lower));
+            var matrix = linalg.Matrix<double>.Build.Dense(numberOfNeronsInThisLayer, numberOfNeronsInPreviusLayer, 0);
+            for (int i = 0; i < matrix.RowCount; i++)
+            {
+                for (int j = 0; j < matrix.ColumnCount; j++)
+                {
+                    matrix[i, j] = lower + rand.NextDouble() * (upper - lower);
+                }
+            }
+            return matrix;
         }
         public NeuralNetwork(List<int> amountOfNodes)
         {
@@ -32,13 +96,12 @@ namespace Lab5
             _amountOfNodes = amountOfNodes;
             _inputLayer = linalg.Vector<double>.Build.Dense(amountOfNodes[0], 0.0);
             _outputLayer = linalg.Vector<double>.Build.Dense(amountOfNodes[amountOfNodes.Count - 1], 0.0);
-            Random rand = new Random(DateTime.Now.Second);
             for (int i = 0; i < amountOfNodes.Count; i++)
             {
                 _layers.Add(linalg.Vector<double>.Build.Dense(amountOfNodes[i], 0.0));
                 if (i != 0)
                 {
-                    _weightMatrices.Add(XavierWeightInitialization(numberOfNeronsInPreviusLayer: amountOfNodes[i - 1], numberOfNeronsInThisLayer: amountOfNodes[i], rand: rand));
+                    _weightMatrices.Add(XavierWeightInitialization(numberOfNeronsInPreviusLayer: amountOfNodes[i - 1], numberOfNeronsInThisLayer: amountOfNodes[i]));
                     _biases.Add(linalg.Vector<double>.Build.Dense(amountOfNodes[i], 0));
                 }
             }
@@ -60,8 +123,17 @@ namespace Lab5
         {
             //sigmoid
             for (int i = 0; i < value.Count; i++)
-            { 
-                value[i] = 1/(1+Math.Exp(-value[i]));
+            {
+                value[i] = 1 / (1 + Math.Exp(-value[i]));
+            }
+            return value;
+        }
+        private linalg.Vector<double> ActivationFunctionDerivative(linalg.Vector<double> value)
+        {
+            //sigmoid*(1-sigmoid)
+            for (int i = 0; i < value.Count; i++)
+            {
+                value[i] = (1 / (1 + Math.Exp(-value[i]))) * (1 - 1 / (1 + Math.Exp(-value[i])));
             }
             return value;
         }
@@ -87,6 +159,61 @@ namespace Lab5
             }
             _outputLayer = _layers[_layers.Count - 1];
             return _outputLayer;
+        }
+        private void SetTrainingData(linalg.Matrix<double> trainingData, int groupBy = 10)
+        {
+            _trainingData = new TrainingData(trainingData, groupBy, inputNodesAmount: _amountOfNodes[0], outputNodesAmount: _amountOfNodes[^-1]);
+        }
+        public void AddTrainingData(linalg.Matrix<double> trainingData, int groupBy = 10)
+        {
+            if (trainingData.ColumnCount != _amountOfNodes[0] + _amountOfNodes[^1])
+            {//list size = (n+m)*amount of data
+                //TODO Try do this error better
+                throw new InvalidOperationException("Training data don`t math NeuralNetwork");
+            }
+            if (_trainingData == null)
+            {
+                SetTrainingData(trainingData, groupBy);
+                return;
+            }
+            _trainingData.AddData(trainingData);
+        }
+        public void TrainNeuralNetwork()
+        {
+            for (int i = 0; i < _trainingData.CountDatasets; i++)
+            {
+                //count mean of Gradients
+                List<linalg.Matrix<double>> gradientWeights = new List<linalg.Matrix<double>>();
+                //same shape as _weightMatrices
+                List<linalg.Vector<double>> gradientBiases = new List<linalg.Vector<double>>();
+                //same shape as _biases
+                for (int j = 1; i < _amountOfNodes.Count; i++)
+                {
+                    gradientWeights.Add(linalg.Matrix<double>.Build.Dense(_amountOfNodes[i - 1], _amountOfNodes[i], 0));
+                    gradientBiases.Add(linalg.Vector<double>.Build.Dense(_amountOfNodes[i], 0));
+                }
+                List<linalg.Vector<double>[]> dataset = _trainingData.DataSets[i];
+                for (int j = 0; j < _trainingData.AmountOfRecordsInDataset; j++)
+                {
+                    var input = dataset[j][0];
+                    var desireOutput = dataset[j][1];
+                    var networkOutput = ActivateNeuralNetwork(input);
+                    var diference = networkOutput - desireOutput;
+                    for (int k = gradientWeights.Count - 1; k >= 0; k--)
+                    {
+                        //double dCdz = 2 * diference * ActivationFunctionDerivative((_weightMatrices[k] * _layers[k] + _biases[k])[]);
+                        for (int l = 0; l < gradientWeights[k].RowCount; l++)
+                        {
+                            for (int m = 0; m < gradientWeights[k].ColumnCount; m++)
+                            {
+
+                            }
+                        }
+                    }
+
+                }
+                //TODO add -gradient
+            }
         }
     }
 }
---
+++ labsIDM/Lab5/TrainingDataGet/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random(DateTime.Now.Second);
+double credit_base;//10000-1000000
+double salary;//15000-3000000
+double persent;// 0.1-2
+bool conections;// True False
+double howmany_haveCredits;// 0 0.5 1 1.5 2
+bool country_factor;// True False
+double country_stability;//0 0.25 0.5 0.75 1
+using StreamWriter file = new("Training.txt", append: true);
+bool cycle = true;
+while (cycle)
+{
+    Console.Clear();
+    credit_base = random.NextDouble()*990000+10000;//10000-1000000
+    salary = random.NextDouble()*2900000+100000;//100000-3000000
+    persent = random.NextDouble() * 2.9 + 0.1;// 0.1-2
+    conections = random.NextDouble()>0.5;// True False
+    howmany_haveCredits = (random.NextDouble()>0.9)?0d:((random.NextDouble() > 0.7) ? 0.5 : ((random.NextDouble() > 0.5) ? 1d : ((random.NextDouble() > 0.3) ? 1.5 : 2d)));// 0 0.5 1 1.5 2
+    country_factor = (random.NextDouble() > 0.9);// True False
+    country_stability = random.NextDouble();//0 0.25 0.5 0.75 1
+    Console.WriteLine($"Credit for {credit_base}\nHis salary {salary}\n" +
+        $"Percent  {persent}\nHe "+(conections?"has":"dont have")+" conection\n" +
+        $"He have {howmany_haveCredits*2} credits\nHe have "+(country_factor?"right":"wrong")+" country\n" +
+        $"Our couuntry stability {country_stability}");
+    ConsoleKeyInfo key = Console.ReadKey();
+    double[] result = new double[3];
+    switch (key.Key)
+    {
+        case ConsoleKey.Escape:
+            cycle = false;
+            break;
+        case ConsoleKey.D1:
+            result[0] = 1d;
+            result[1] = 0;
+            result[2] = 0;
+            break;
+        case ConsoleKey.D2:
+            result[0] = 0.5;
+            result[1] = 0.5;
+            result[2] = 0;
+            break;
+        case ConsoleKey.D3:
+            result[0] = 0;
+            result[1] = 1;
+            result[2] = 0;
+            break;
+        case ConsoleKey.D4:
+            result[0] = 0;
+            result[1] = 0.5;
+            result[2] = 0.5;
+            break;
+        case ConsoleKey.D5:
+            result[0] = 0;
+            result[1] = 0;
+            result[2] = 1;
+            break;
+        default:
+            continue;
+            break;
+    }
+    double result_credit = credit_base * (1 + persent) / salary / 3;
+    double result_Creditscount = howmany_haveCredits / 2.0d;
+    string resultstr = result_credit.ToString() + "\\" + (persent / 2.0).ToString() + "\\" + (conections ? 1 : 0).ToString() + "\\" + result_Creditscount.ToString() + "\\" + (country_factor ? 1 : 0).ToString() + "\\" + country_stability.ToString() + "|" + result[0].ToString() + "\\" + result[1].ToString() + "\\" + result[2].ToString();
+    await file.WriteLineAsync(resultstr);
+}
